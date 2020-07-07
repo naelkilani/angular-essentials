251aa424d1d138704bd76d903535965b944a1d51
Nael Kilani <naelkilanii@gmail.com>
2020-07-07 23:38:29 +0300

    Add first component.

---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+// deaerator function
+@Component({
+    selector: 'courses',
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+
+}
