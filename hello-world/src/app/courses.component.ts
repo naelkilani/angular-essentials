2b568d112d7cf6f3a5ab500a458500146b27d2ee
Nael Kilani <naelkilanii@gmail.com>
2020-07-09 02:01:58 +0300

    working with pipes.

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -23,9 +23,14 @@ import { Component } from '@angular/core';
             (click)=[onSave($event)]
         >Save</button>
     </div>
-    <input (keyUp.enter)="onEnter1($event)" />
-    <input #email (keyUp.enter)="onEnter2(email.value)" />
-    <input [(ngModel)]="name" (keyUp.enter)="onEnter3()" />  
+    <input (keyUp.enter)="onEnter1($event)" /> <br/>
+    <input #email (keyUp.enter)="onEnter2(email.value)" /> <br/>
+    <input [(ngModel)]="name" (keyUp.enter)="onEnter3()" /> <br/>
+    {{ course.title | uppercase }} <br/>
+    {{ course.students | number }} <br/>
+    {{ course.rating | number:'2.1-1' }} <br/>
+    {{ course.price | currency:'USD':true:'3.2-2' }} <br/>
+    {{ course.releaseDate | date:'shortDate' }}
     `
 })
 export class CoursesComponent {
@@ -35,6 +40,13 @@ export class CoursesComponent {
     isActive = false;
     colSpan = 4;
     name = "default value";
+    course = {
+        title: "The Complete Angular Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2016, 3, 1)
+    }
 
     constructor(coursesService: CoursesService) {
         this.courses = coursesService.getCourses();
