e9d094b1f4c1acab4834aac68935017e7dac5bfb
Nael Kilani <naelkilanii@gmail.com>
2020-07-14 22:26:26 +0300

    simple bootstrap form.

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ZippyComponent } from './zippy/zippy.component';
     PanelComponent,
     LikeComponent,
     InputFormatDirective,
-    ZippyComponent
+    ZippyComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
