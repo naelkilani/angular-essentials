43fd54153fdd1547c1fbcd466e950200e62d3420
Nael Kilani <naelkilanii@gmail.com>
2020-07-18 18:52:35 +0300

    Routing: - Working with qurery parameters.

--- hello-world/src/app/github-followers/github-followers.component.ts
+++ hello-world/src/app/github-followers/github-followers.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-github-followers',
   templateUrl: './github-followers.component.html',
   styleUrls: ['./github-followers.component.css']
 })
-export class GithubFollowersComponent{
+export class GithubFollowersComponent implements OnInit{
+  constructor(private routes: ActivatedRoute) {
+  }
+  
   followers = [
     { id: 1, name: "Ahmad" },
     { id: 2, name: "Ali"}
   ];
+
+  ngOnInit(): void {
+    this.routes.queryParamMap.subscribe(params => {
+      console.log(params.keys);
+    });
+  }
+
 }
